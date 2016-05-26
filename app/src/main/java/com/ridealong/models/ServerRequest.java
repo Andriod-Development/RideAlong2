f57c82120b00668bfa9063dd9a5c9b7bf6c00f22
ven007 <mvenkat1177@gmail.com>
2016-05-26 14:47:38 -0700

    Push the server interfaces and User model

---
+++ app/src/main/java/com/ridealong/models/ServerRequest.java
@@ -0,0 +1,18 @@
+package com.ridealong.models;
+
+/**
+ * Created by HP on 5/20/2016.
+ */
+public class ServerRequest {
+
+    private String operation;
+    private User user;
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ app/src/main/java/com/ridealong/models/ServerResponse.java
@@ -0,0 +1,23 @@
+package com.ridealong.models;
+
+/**
+ * Created by HP on 5/20/2016.
+ */
+public class ServerResponse {
+
+    private String result;
+    private String message;
+    private User user;
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
