b322b2e1faae0e29e5bdaae38cd41a0a699ebe77
PrasanthReddyYerradoddi <prasanth.reddy.93@gmail.com>
2016-05-26 17:55:59 -0700

    ddetails

--- app/src/main/java/com/ridealong/DriverActivity.java
+++ app/src/main/java/com/ridealong/DriverActivity.java
@@ -3,15 +3,27 @@ package com.ridealong;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
 
 //car details for passenger
 public class DriverActivity extends AppCompatActivity {
+
+    EditText dfirstname, dto, dfrom, ddate, dtime, dmodel, dlicense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        dfirstname = (EditText) findViewById(R.id.dfirstname);
+        dto = (EditText) findViewById(R.id.dto);
+        dfrom = (EditText) findViewById(R.id.dfrom);
+        ddate = (EditText) findViewById(R.id.ddate);
+        dtime = (EditText) findViewById(R.id.dtime);
+        dmodel = (EditText) findViewById(R.id.dmodel);
+        dlicense = (EditText) findViewById(R.id.dlicense);
+
+                Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
--- app/src/main/java/com/ridealong/DriverActivityFragment.java
+++ app/src/main/java/com/ridealong/DriverActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,9 +15,15 @@ public class DriverActivityFragment extends Fragment {
     public DriverActivityFragment() {
     }
 
+    EditText dfirstName;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_driver, container, false);
+        View view = inflater.inflate(R.layout.fragment_driver, container, false);
+
+
+
+        return view;
     }
 }
