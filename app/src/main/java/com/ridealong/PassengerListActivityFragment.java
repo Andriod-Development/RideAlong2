bc1475ad2eb64712e55bb3aa1d31011123d8f9f7
PrasanthReddyYerradoddi <prasanth.reddy.93@gmail.com>
2016-05-29 17:20:23 -0700

    list

--- app/src/main/java/com/ridealong/PassengerListActivityFragment.java
+++ app/src/main/java/com/ridealong/PassengerListActivityFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,9 +20,17 @@ public class PassengerListActivityFragment extends Fragment {
     public PassengerListActivityFragment() {
     }
 
+    ArrayAdapter adapter;
+    List<String> x = new ArrayList<>(Arrays.asList("1", "2"));
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_passenger_list, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_passenger_list, container, false);
+        ListView listView = (ListView) view.findViewById(R.id.passengerList);
+        adapter = new ArrayAdapter<String >(getActivity(),R.layout.passenger_list_items, R.id.passenger_list_item, x);
+        listView.setAdapter(adapter);
+        return view;
     }
 }
