f42a967c01733bbebe09f2c6d5c1cba70cb6bad3
PrasanthReddyYerradoddi <prasanth.reddy.93@gmail.com>
2016-06-01 01:12:15 -0700

    view map

--- app/src/main/java/com/ridealong/PassengerDetailActivity.java
+++ app/src/main/java/com/ridealong/PassengerDetailActivity.java
@@ -15,6 +15,8 @@ public class PassengerDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_passenger_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
     }
 
 }
